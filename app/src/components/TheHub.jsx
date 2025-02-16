a5973dbe4cca2eb10880e240c5767d2be58af5cb
Raymond Robinson <rayissach@gmail.com>
2025-02-16 12:17:16 -0500

    Initializing Hub page for apps to access

---
+++ app/src/components/TheHub.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function TheHub() {
+
+    return(
+        <div>
+            <ul>
+                <li>GitHub</li>
+                <li>LinkedIn</li>
+                <li>Neetcode</li>
+                <li>greatfrontend</li>
+            </ul>
+        </div>
+    )
+}
